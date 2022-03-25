2935cffbb8cd556197bfc024bdd16d42cfe4a7b4
93wilsonlu <93wilsonlu@gmail.com>
2022-03-25 15:53:17 +0800

    做導覽頁

---
+++ three_topic_story/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap5
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+# import all app
+from three_topic_story.main import main_blueprint
+app.register_blueprint(main_blueprint)
