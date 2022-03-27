2efd8d4b3688bbcb627c3e127ff30aef2b24bfc3
93wilsonlu <93wilsonlu@gmail.com>
2022-03-27 18:00:27 +0800

    更新 heroku 上的 postgresql

--- three_topic_story/account/model.py
+++ three_topic_story/account/model.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'User'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
