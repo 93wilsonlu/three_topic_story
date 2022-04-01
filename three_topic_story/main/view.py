5689cf6f0c3a05698ec5e3fe552d838ff1bf94fc
93wilsonlu <93wilsonlu@gmail.com>
2022-04-01 15:44:39 +0800

    新增 profile 頁面

--- three_topic_story/main/view.py
+++ three_topic_story/main/view.py
@@ -1,6 +1,11 @@
 from . import main
 from flask import render_template
+from three_topic_story.account.model import User
 
 @main.route('/')
 def index():
     return render_template('main/index.html')
+
+@main.route('/profile/<string:username>')
+def profile(username):
+    return render_template('main/profile.html', target_user=User.query.filter_by(username=username).first())
