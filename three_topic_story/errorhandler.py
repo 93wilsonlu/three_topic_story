79d81c3b67ccea8a81a972b50ab434b1c48d6661
93wilsonlu <93wilsonlu@gmail.com>
2022-04-01 12:58:12 +0800

    完成 404 handler

---
+++ three_topic_story/errorhandler.py
@@ -0,0 +1,17 @@
+from flask_login import current_user
+from flask import request, flash, redirect, url_for, render_template
+
+def init_errorhandler(app):
+    @app.before_app_request
+    def before_request():
+        if (current_user.is_authenticated and
+                not current_user.confirm and
+                request.endpoint != 'main.index' and
+                'account' not in request.endpoint and
+                request.endpoint != 'static'):
+            flash('請驗證您的帳號')
+            return redirect(url_for('account.setting'))
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
