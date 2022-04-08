82f8e2fff68bdbf944cc9762fbca6337753d4e9b
93wilsonlu <93wilsonlu@gmail.com>
2022-04-08 12:51:02 +0800

    完成文章列表

--- three_topic_story/main/view.py
+++ three_topic_story/main/view.py
@@ -1,6 +1,7 @@
-from . import main
+from three_topic_story.main import main
 from flask import render_template
 from three_topic_story.account.model import User
+from three_topic_story.post.model import Post
 
 
 @main.route('/')
@@ -11,3 +12,11 @@ def index():
 @main.route('/profile/<string:username>')
 def profile(username):
     return render_template('main/profile.html', target_user=User.query.filter_by(username=username).first())
+
+
+@main.route('/post_list/')
+@main.route('/post_list/<int:page>/')
+def post_list(page=1):
+    posts = Post.query.filter_by(
+        private=False).paginate(page, 1, False)
+    return render_template('main/post_list.html', posts=posts)
